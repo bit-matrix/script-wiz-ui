cfd1d640e0ad2d2e46dc2db13ec450e82ca11795
Umut ISIK <uuisik@gmail.com>
2021-05-13 01:54:43 +0300

    scriptWix parse, compile function, outputs

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,46 @@
 /* eslint-disable no-template-curly-in-string */
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Editor, { useMonaco } from "@monaco-editor/react";
 import * as Monaco from "monaco-editor/esm/vs/editor/editor.api";
+import * as scriptWiz from "@script-wiz/lib";
 import { scriptWizEditor } from "./editor/utils/constant";
 import themeOptions from "./editor/options/themeOptions";
 import editorOptions from "./editor/options/editorOptions";
 import * as languageOptions from "./editor/options/languageOptions";
 import "./App.css";
+import IStackData from "../../script-wiz-lib/dist/model/IStackData";
+
+const initialStackDataList: Array<IStackData | undefined> = [];
 
 function App() {
+  const [stackDataList, setStackDataList] = useState<Array<IStackData | undefined>>(initialStackDataList);
+
   const monaco = useMonaco();
+
+  const compile = (lines: string[]) => {
+    setStackDataList(initialStackDataList);
+    scriptWiz.clearStack();
+    const newStackDataList: Array<IStackData | undefined> = [];
+
+    for (let i = 0; i < lines.length; i++) {
+      const line = lines[i];
+      let newStackData: IStackData | undefined = undefined;
+
+      try {
+        if (line !== "") {
+          const libStackDataList = scriptWiz.parse(line);
+          newStackData = libStackDataList.main[libStackDataList.main.length - 1];
+        }
+        newStackDataList.push(newStackData);
+      } catch (e) {
+        newStackData = e;
+        newStackDataList.push(newStackData);
+        continue;
+      }
+    }
+
+    setStackDataList(newStackDataList);
+  };
 
   const onChangeEditor = (value: string | undefined, ev: Monaco.editor.IModelContentChangedEvent) => {
     if (value) {
@@ -19,6 +50,7 @@ function App() {
       lines = lines.map((line) => line.replaceAll("\r", ""));
       lines = lines.map((line) => line.replaceAll("\t", ""));
       console.log(lines);
+      compile(lines);
     }
   };
 
@@ -46,8 +78,36 @@ function App() {
     }
   }, [monaco]);
 
+  const output = () => {
+    const outputStrings: string[] = [];
+    stackDataList.forEach((d, i) => {
+      let lineOutput = "";
+      for (let j = 0; j < i + 1; j++) {
+        const data = stackDataList[j];
+        lineOutput += data ? (data.input ? data.input + "(" + data.byteValue : data) + ")" : "";
+        lineOutput += " ";
+      }
+      outputStrings.push(lineOutput);
+    });
+    return outputStrings;
+  };
+
   if (monaco != null) {
-    return <Editor width="50vw" options={editorOptions} language={scriptWizEditor.LANGUAGE} height="100vh" theme={scriptWizEditor.THEME} onChange={onChangeEditor} />;
+    return (
+      <div style={{ height: "100vh", width: "100vw" }}>
+        <div style={{ float: "left", height: "100%", width: "50%" }}>
+          <Editor height="100vh" width="100%" options={editorOptions} language={scriptWizEditor.LANGUAGE} theme={scriptWizEditor.THEME} onChange={onChangeEditor} />
+        </div>
+        <div style={{ float: "left", height: "100%", width: "50%" }}>
+          {output().map((o) => (
+            <>
+              <span>{o}</span>
+              <br />
+            </>
+          ))}
+        </div>
+      </div>
+    );
   }
 
   return null;
