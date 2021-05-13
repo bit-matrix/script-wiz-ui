03e462673d00fb82239a3fc6862a3accf2ebb8a2
afarukcali <afarukcali@gmail.com>
2021-05-14 01:26:04 +0300

    node sass version 5.0.0

--- src/components/ScriptEditorOutput/ScriptEditorOutput.tsx
+++ src/components/ScriptEditorOutput/ScriptEditorOutput.tsx
@@ -4,43 +4,43 @@ import "./ScriptEditorOutput.scss";
 import { Button, Tooltip, Whisper } from "rsuite";
 
 interface IScriptEditorInput {
-    stackDataList: Array<IStackData | undefined>;
+  stackDataList: Array<IStackData | undefined>;
 }
 
 const ScriptEditorOutput: React.FC<IScriptEditorInput> = ({ stackDataList }) => {
-    const [outputs, setOutputs] = useState<string[]>([]);
+  const [outputs, setOutputs] = useState<string[]>([]);
 
-    useEffect(() => {
-        const outputStrings: string[] = [];
-        stackDataList.forEach((d, i) => {
-            let lineOutput = "";
-            for (let j = 0; j < i + 1; j++) {
-                const data = stackDataList[j];
-                lineOutput += data ? (data.input ? data.input + "(" + data.byteValue : data) + ")" : "";
-                lineOutput += " ";
-            }
-            outputStrings.push(lineOutput);
-        });
+  useEffect(() => {
+    const outputStrings: string[] = [];
+    stackDataList.forEach((d, i) => {
+      let lineOutput = "";
+      for (let j = 0; j < i + 1; j++) {
+        const data = stackDataList[j];
+        lineOutput += data ? (data.input ? data.input + "(" + data.byteValue : data) + ")" : "";
+        lineOutput += " ";
+      }
+      outputStrings.push(lineOutput);
+    });
 
-        setOutputs(outputStrings);
-    }, [stackDataList]);
+    setOutputs(outputStrings);
+  }, [stackDataList]);
 
-    return (
-        <>
-            {outputs.map((o, index) => (
-                <div className="script-editor-output-main" key={index.toString()}>
-                    <span key={`${index.toString() + "index"}`} className="editor-output-text-page-number">
-                        {index + 1}
-                    </span>
+  return (
+    <>
+      {outputs.map((o, index) => (
+        <div className="script-editor-output-main" key={index.toString()}>
+          <span key={`${index.toString() + "index"}`} className="editor-output-text-page-number">
+            {index + 1}
+          </span>
 
-                    <span key={`${index.toString() + "text"}`} className="editor-output-text">
-                        {o}
-                    </span>
-                    <br />
-                </div>
-            ))}
-        </>
-    );
+          <span key={`${index.toString() + "text"}`} className="editor-output-text">
+            {o}
+          </span>
+          <br />
+        </div>
+      ))}
+    </>
+  );
 };
 
 export default ScriptEditorOutput;
