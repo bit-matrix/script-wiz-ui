a526b974a7e99f86cb87d0b8407e4382b6b75738
afarukcali <afarukcali@gmail.com>
2021-05-14 01:38:49 +0300

    Rsuite dark mode import.

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,13 @@ import ReactDOM from "react-dom";
 import "./index.scss";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import "rsuite/lib/styles/index.less";
+import "rsuite/dist/styles/rsuite-dark.css";
 
 ReactDOM.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>,
-    document.getElementById("root"),
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
