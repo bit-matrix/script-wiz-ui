13a090bcdee95ef2e1c2268bf62e98c206ca6b20
hüseyin sarıtaş <huseyinsaritas@test-MacBook-Pro.local>
2021-05-15 15:25:29 +0300

    ScriptNavbar usage

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 /* eslint-disable no-template-curly-in-string */
 import React from "react";
 import "./App.scss";
+import ScriptNavbar from "./components/ScriptNavbar/ScriptNavbar";
 import ScriptEditor from "./components/ScriptEditor/ScriptEditor";
 
 const App = () => {
-    return <ScriptEditor />;
+    return (
+        <div>
+            <ScriptNavbar />
+            <ScriptEditor />
+        </div>
+    );
 };
 
 export default App;
