161c18de8e8d5ae66cc24783989c85e5b17e2624
hüseyin sarıtaş <huseyinsaritas@test-MacBook-Pro.local>
2021-05-15 15:25:02 +0300

    Script Header usage

--- src/components/ScriptEditor/ScriptEditor.tsx
+++ src/components/ScriptEditor/ScriptEditor.tsx
@@ -4,54 +4,58 @@ import * as scriptWiz from "@script-wiz/lib";
 import ScriptEditorInput from "./ScriptEditorInput/ScriptEditorInput";
 import ScriptEditorOutput from "./ScriptEditorOutput/ScriptEditorOutput";
 import "./ScriptEditor.scss";
+import ScriptEditorHeader from "./ScriptEditorHeader/ScriptEditorHeader";
 
 const initialLineStackDataListArray: Array<Array<IStackData>> = [];
 const initialLastStackDataList: Array<IStackData> = [];
 
 const ScriptEditor = () => {
-  const [errorMessage, setErrorMessage] = useState<string | undefined>();
-  const [lineStackDataListArray, setLineStackDataListArray] = useState<Array<Array<IStackData>>>(initialLineStackDataListArray);
-  const [lastStackDataList, setLastStackDataList] = useState<Array<IStackData>>(initialLastStackDataList);
+    const [errorMessage, setErrorMessage] = useState<string | undefined>();
+    const [lineStackDataListArray, setLineStackDataListArray] = useState<Array<Array<IStackData>>>(initialLineStackDataListArray);
+    const [lastStackDataList, setLastStackDataList] = useState<Array<IStackData>>(initialLastStackDataList);
 
-  const compile = (lines: string[]) => {
-    setErrorMessage(undefined);
-    setLineStackDataListArray(initialLineStackDataListArray);
-    setLastStackDataList(lastStackDataList);
-    scriptWiz.clearStack();
+    const compile = (lines: string[]) => {
+        setErrorMessage(undefined);
+        setLineStackDataListArray(initialLineStackDataListArray);
+        setLastStackDataList(lastStackDataList);
+        scriptWiz.clearStack();
 
-    const newLineStackDataListArray: Array<Array<IStackData>> = [];
-    let newLastStackDataList: Array<IStackData> = [];
+        const newLineStackDataListArray: Array<Array<IStackData>> = [];
+        let newLastStackDataList: Array<IStackData> = [];
 
-    for (let i = 0; i < lines.length; i++) {
-      const line = lines[i];
+        for (let i = 0; i < lines.length; i++) {
+            const line = lines[i];
 
-      try {
-        if (line !== "") {
-          newLastStackDataList = scriptWiz.parse(line).main;
-          newLineStackDataListArray.push(newLastStackDataList);
-        } else {
-          newLineStackDataListArray.push([]);
+            try {
+                if (line !== "") {
+                    newLastStackDataList = scriptWiz.parse(line).main;
+                    newLineStackDataListArray.push(newLastStackDataList);
+                } else {
+                    newLineStackDataListArray.push([]);
+                }
+            } catch (e) {
+                setErrorMessage(e);
+                break;
+            }
         }
-      } catch (e) {
-        setErrorMessage(e);
-        break;
-      }
-    }
 
-    setLineStackDataListArray(newLineStackDataListArray);
-    setLastStackDataList(newLastStackDataList);
-  };
+        setLineStackDataListArray(newLineStackDataListArray);
+        setLastStackDataList(newLastStackDataList);
+    };
 
-  return (
-    <div className="script-editor-main-div">
-      <div className="script-editor-sub-item">
-        <ScriptEditorInput onChangeScriptEditorInput={compile} />
-      </div>
-      <div className="script-editor-sub-item">
-        <ScriptEditorOutput lastStackDataList={lastStackDataList} lineStackDataListArray={lineStackDataListArray} errorMessage={errorMessage} />
-      </div>
-    </div>
-  );
+    return (
+        <div className="script-editor-main-div">
+            <ScriptEditorHeader />
+            <div className="script-editor-container">
+                <div className="script-editor-sub-item">
+                    <ScriptEditorInput onChangeScriptEditorInput={compile} />
+                </div>
+                <div className="script-editor-sub-item">
+                    <ScriptEditorOutput lastStackDataList={lastStackDataList} lineStackDataListArray={lineStackDataListArray} errorMessage={errorMessage} />
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default ScriptEditor;
