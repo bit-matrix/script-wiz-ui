fdb41044be9a223850a859aeb417b3d1cec3db35
afarukcali <afarukcali@gmail.com>
2021-05-15 18:22:58 +0300

    Navbar items urls fixed.

--- src/components/ScriptNavbar/ScriptNavbar.tsx
+++ src/components/ScriptNavbar/ScriptNavbar.tsx
@@ -1,57 +1,59 @@
+/* eslint-disable jsx-a11y/alt-text */
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from "react";
 import { Dropdown, Icon } from "rsuite";
 import logo from "../../images/transparent_white.png";
 import "./ScriptNavbar.scss";
 
 const ScriptNavbar = () => {
-    return (
-        <div className="script-editor-header-bar">
-            <div className="script-editor-header-left-section">
-                <div>
-                    <img className="script-wiz-logo" src={logo} />
-                </div>
-                <a href="https://github.com/bit-matrix" className="script-editor-header-icon-item">
-                    <Icon icon="github" />
-                    <span className="script-editor-header-icon-item-text">Github</span>
-                </a>
-                <a href="https://twitter.com/bitmatrix_" className="script-editor-header-icon-item">
-                    <Icon icon="twitter" />
-                    <span className="script-editor-header-icon-item-text">Npm</span>
-                </a>
-                <a href="https://twitter.com/bitmatrix_" className="script-editor-header-icon-item">
-                    <Icon icon="twitter" />
-                    <span className="script-editor-header-icon-item-text">Twitter</span>
-                </a>
-                <a className="script-editor-header-icon-item">
-                    <Icon icon="medium" />
-                    <span className="script-editor-header-icon-item-text">Medium</span>
-                </a>
-                <a href="https://t.me/bitmatrix_community" className="script-editor-header-icon-item">
-                    <Icon icon="telegram" />
-                    <span className="script-editor-header-icon-item-text">Telegram</span>
-                </a>
-            </div>
-            <div className="script-editor-header-right-section">
-                <Dropdown
-                    className="script-editor-header-right-section-dropdown"
-                    title={
-                        <div className="dropdown-item">
-                            <span>Liquid Network</span>
-                        </div>
-                    }
-                    activeKey="a"
-                >
-                    <Dropdown.Item eventKey="a">
-                        {
-                            <div className="dropdown-item">
-                                <span>Bitcoin Network</span>
-                            </div>
-                        }
-                    </Dropdown.Item>
-                </Dropdown>
-            </div>
+  return (
+    <div className="script-editor-header-bar">
+      <div className="script-editor-header-left-section">
+        <div>
+          <img className="script-wiz-logo" src={logo} />
         </div>
-    );
+        <a href="https://github.com/bit-matrix/script-wiz-lib" target="_blank" className="script-editor-header-icon-item" rel="noreferrer">
+          <Icon icon="github" />
+          <span className="script-editor-header-icon-item-text">Github</span>
+        </a>
+        <a href="https://www.npmjs.com/package/@script-wiz/lib" target="_blank" className="script-editor-header-icon-item" rel="noreferrer">
+          <Icon icon="project" />
+          <span className="script-editor-header-icon-item-text">Npm</span>
+        </a>
+        <a href="https://twitter.com/script_wizard" target="_blank" className="script-editor-header-icon-item" rel="noreferrer">
+          <Icon icon="twitter" />
+          <span className="script-editor-header-icon-item-text">Twitter</span>
+        </a>
+        <a className="script-editor-header-icon-item">
+          <Icon icon="medium" />
+          <span className="script-editor-header-icon-item-text">Medium</span>
+        </a>
+        <a href="https://t.me/scriptwizard" target="_blank" className="script-editor-header-icon-item" rel="noreferrer">
+          <Icon icon="telegram" />
+          <span className="script-editor-header-icon-item-text">Telegram</span>
+        </a>
+      </div>
+      <div className="script-editor-header-right-section">
+        <Dropdown
+          className="script-editor-header-right-section-dropdown"
+          title={
+            <div className="dropdown-item">
+              <span>Liquid Network</span>
+            </div>
+          }
+          activeKey="a"
+        >
+          <Dropdown.Item disabled eventKey="a">
+            {
+              <div className="dropdown-item">
+                <span>Bitcoin Network</span>
+              </div>
+            }
+          </Dropdown.Item>
+        </Dropdown>
+      </div>
+    </div>
+  );
 };
 
 export default ScriptNavbar;
