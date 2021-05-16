e2e1e8e5c40d0b44e9cce4b084e3c11a22908ba5
afarukcali <afarukcali@gmail.com>
2021-05-16 20:34:35 +0300

    Navbar mobile design

--- src/components/ScriptNavbar/ScriptNavbar.tsx
+++ src/components/ScriptNavbar/ScriptNavbar.tsx
@@ -6,54 +6,84 @@ import logo from "../../images/transparent_white.png";
 import "./ScriptNavbar.scss";
 
 const ScriptNavbar = () => {
-  return (
-    <div className="script-editor-header-bar">
-      <div className="script-editor-header-left-section">
-        <div>
-          <img className="script-wiz-logo" src={logo} />
-        </div>
-        <a href="https://github.com/bit-matrix/script-wiz-lib" target="_blank" className="script-editor-header-icon-item" rel="noreferrer">
-          <Icon icon="github" />
-          <span className="script-editor-header-icon-item-text">Github</span>
-        </a>
-        <a href="https://www.npmjs.com/package/@script-wiz/lib" target="_blank" className="script-editor-header-icon-item" rel="noreferrer">
-          <Icon icon="project" />
-          <span className="script-editor-header-icon-item-text">Npm</span>
-        </a>
-        <a href="https://twitter.com/script_wizard" target="_blank" className="script-editor-header-icon-item" rel="noreferrer">
-          <Icon icon="twitter" />
-          <span className="script-editor-header-icon-item-text">Twitter</span>
-        </a>
-        <a className="script-editor-header-icon-item">
-          <Icon icon="medium" />
-          <span className="script-editor-header-icon-item-text">Medium</span>
-        </a>
-        <a href="https://t.me/scriptwizard" target="_blank" className="script-editor-header-icon-item" rel="noreferrer">
-          <Icon icon="telegram" />
-          <span className="script-editor-header-icon-item-text">Telegram</span>
-        </a>
-      </div>
-      <div className="script-editor-header-right-section">
-        <Dropdown
-          className="script-editor-header-right-section-dropdown"
-          title={
-            <div className="dropdown-item">
-              <span>Liquid Network</span>
+    return (
+        <div className="script-editor-header-bar">
+            <div className="script-editor-header-left-section">
+                <div>
+                    <img className="script-wiz-logo" src={logo} />
+                </div>
+                <a
+                    href="https://github.com/bit-matrix/script-wiz-lib"
+                    target="_blank"
+                    className="script-editor-header-icon-item"
+                    rel="noreferrer"
+                >
+                    <Icon icon="github" />
+                    <span className="script-editor-header-icon-item-text hidden-mobile">
+                        Github
+                    </span>
+                </a>
+                <a
+                    href="https://www.npmjs.com/package/@script-wiz/lib"
+                    target="_blank"
+                    className="script-editor-header-icon-item"
+                    rel="noreferrer"
+                >
+                    <Icon icon="project" />
+                    <span className="script-editor-header-icon-item-text  hidden-mobile">
+                        Npm
+                    </span>
+                </a>
+                <a
+                    href="https://twitter.com/script_wizard"
+                    target="_blank"
+                    className="script-editor-header-icon-item"
+                    rel="noreferrer"
+                >
+                    <Icon icon="twitter" />
+                    <span className="script-editor-header-icon-item-text  hidden-mobile">
+                        Twitter
+                    </span>
+                </a>
+                <a className="script-editor-header-icon-item">
+                    <Icon icon="medium" />
+                    <span className="script-editor-header-icon-item-text  hidden-mobile">
+                        Medium
+                    </span>
+                </a>
+                <a
+                    href="https://t.me/scriptwizard"
+                    target="_blank"
+                    className="script-editor-header-icon-item"
+                    rel="noreferrer"
+                >
+                    <Icon icon="telegram" />
+                    <span className="script-editor-header-icon-item-text hidden-mobile">
+                        Telegram
+                    </span>
+                </a>
             </div>
-          }
-          activeKey="a"
-        >
-          <Dropdown.Item disabled eventKey="a">
-            {
-              <div className="dropdown-item">
-                <span>Bitcoin Network</span>
-              </div>
-            }
-          </Dropdown.Item>
-        </Dropdown>
-      </div>
-    </div>
-  );
+            <div className="script-editor-header-right-section">
+                <Dropdown
+                    className="script-editor-header-right-section-dropdown"
+                    title={
+                        <div className="dropdown-item">
+                            <span>Liquid Network</span>
+                        </div>
+                    }
+                    activeKey="a"
+                >
+                    <Dropdown.Item disabled eventKey="a">
+                        {
+                            <div className="dropdown-item">
+                                <span>Bitcoin Network</span>
+                            </div>
+                        }
+                    </Dropdown.Item>
+                </Dropdown>
+            </div>
+        </div>
+    );
 };
 
 export default ScriptNavbar;
