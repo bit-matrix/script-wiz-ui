64eb73b1368b436c63c3c9d46a7b512e9416997b
afarukcali <afarukcali@gmail.com>
2021-05-16 13:55:51 +0300

    Script editor state set in different scope.

--- src/index.tsx
+++ src/index.tsx
@@ -6,9 +6,9 @@ import reportWebVitals from "./reportWebVitals";
 import "rsuite/dist/styles/rsuite-dark.css";
 
 ReactDOM.render(
-  <React.StrictMode>
+  <>
     <App />
-  </React.StrictMode>,
+  </>,
   document.getElementById("root")
 );
 
