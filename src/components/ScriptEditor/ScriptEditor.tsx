9bf9d8d799df0b566ce6c4ecee11bd1fd2d367dc
hüseyin sarıtaş <huseyinsaritas@test-MacBook-Pro.local>
2021-08-12 14:54:28 +0300

    ScriptEditor: Compile Modal removed

--- src/components/ScriptEditor/ScriptEditor.tsx
+++ src/components/ScriptEditor/ScriptEditor.tsx
@@ -2,25 +2,15 @@ import React, { useEffect, useState } from 'react';
 import ScriptEditorInput from './ScriptEditorInput/ScriptEditorInput';
 import ScriptEditorOutput from './ScriptEditorOutput/ScriptEditorOutput';
 import ScriptEditorHeader from './ScriptEditorHeader/ScriptEditorHeader';
-import { Button, Form, FormGroup, Icon, Input, InputGroup, Modal, Radio, RadioGroup, Tooltip, Whisper } from 'rsuite';
 import { convertEditorLines } from '../../helper';
-import { ScriptWiz, VM_NETWORK, WizData, tapRoot } from '@script-wiz/lib';
+import { ScriptWiz, VM_NETWORK, WizData } from '@script-wiz/lib';
 import { initialBitcoinEditorValue, initialLiquidEditorValue } from './ScriptEditorInput/initialEditorValue';
+import CompileModal from '../CompileModal/CompileModal';
 import './ScriptEditor.scss';
 
 type Props = {
   scriptWiz: ScriptWiz;
 };
-
-enum KeyPath {
-  UNKNOWN = 'unknown',
-  CUSTOM = 'custom',
-}
-
-enum TapleafVersion {
-  DEFAULT = 'default',
-  CUSTOM = 'custom',
-}
 
 const initialLineStackDataListArray: Array<Array<WizData>> = [];
 const initialLastStackDataList: Array<WizData> = [];
@@ -35,17 +25,6 @@ const ScriptEditor: React.FC<Props> = ({ scriptWiz }) => {
     data?: string;
   }>({ show: false });
 
-  const [keyPath, setKeyPath] = useState<KeyPath>(KeyPath.UNKNOWN);
-  const [tapleafVersion, setTapleafVersion] = useState<TapleafVersion>(TapleafVersion.DEFAULT);
-  const [pubKeyInput, setPubKeyInput] = useState<string>('');
-  const [tapleafInput, setTapleafInput] = useState<string>('');
-
-  const [tweakedResult, setTweakedResult] = useState<string>('');
-
-  const pubkeyDefaultValue: string = '021dae61a4a8f841952be3a511502d4f56e889ffa0685aa0098773ea2d4309f624';
-
-  const tapleafDefaultValue: string = '0xc0';
-
   useEffect(() => {
     let unmounted = false;
 
@@ -58,7 +37,7 @@ const ScriptEditor: React.FC<Props> = ({ scriptWiz }) => {
       unmounted = true;
     };
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [scriptWiz.vm.network, pubKeyInput, keyPath]);
+  }, [scriptWiz.vm.network]);
 
   const compile = (lines: string[]) => {
     scriptWiz.clearStackDataList();
@@ -129,137 +108,9 @@ const ScriptEditor: React.FC<Props> = ({ scriptWiz }) => {
     setCompileModalData({ show: true, data: compileScript });
   };
 
-  useEffect(() => {
-    const script = compileModalData.data?.substr(2) || scriptWiz.compile().substr(2);
-    let pubkey = pubkeyDefaultValue;
-    let version = undefined;
-
-    if (keyPath === KeyPath.CUSTOM) pubkey = pubKeyInput;
-
-    if (tapleafVersion === TapleafVersion.CUSTOM) version = tapleafInput.substr(2);
-
-    if (
-      (pubkey.length >= 64 && tapleafVersion === TapleafVersion.DEFAULT) ||
-      (tapleafVersion === TapleafVersion.CUSTOM && version !== undefined && version?.length >= 2)
-    ) {
-      try {
-        const result = tapRoot(pubkey, script, version);
-        setTweakedResult(result);
-      } catch {
-        setTweakedResult('Invalid result');
-      }
-    } else if (pubKeyInput.length < 64 && pubKeyInput.length > 0) {
-      setTweakedResult('Invalid Result');
-    } else if (tapleafVersion === TapleafVersion.CUSTOM && version !== undefined && version.length < 4) {
-      setTweakedResult('Invalid Result');
-    } else {
-      setTweakedResult('');
-    }
-  }, [compileModalData.data, keyPath, pubKeyInput, scriptWiz, tapleafInput, tapleafVersion]);
-
   return (
     <>
-      <Modal size="sm" show={compileModalData.show} backdrop={false} onHide={() => setCompileModalData({ show: false })}>
-        <Modal.Header />
-        <Modal.Body className="compile-modal-body scroll">
-          <h5 className="compile-modal-item">Compile Result</h5>
-          <p className="compile-data-p">{compileModalData.data}</p>
-          <Form fluid>
-            <h5 className="compile-modal-item">Taproot Output</h5>
-            <div>
-              <div className="compile-modal-label">Key-path:</div>
-              <RadioGroup
-                className="compile-modal-radio-group"
-                inline
-                value={keyPath}
-                onChange={(value: KeyPath) => {
-                  setKeyPath(value);
-                }}
-              >
-                <Radio value={KeyPath.UNKNOWN}>Unknown discrete logarithm</Radio>
-                <Radio value={KeyPath.CUSTOM}>Custom</Radio>
-              </RadioGroup>
-              <div className="compile-modal-item">
-                <div className="compile-modal-label">Public Key as HEX string:</div>
-                <Input
-                  disabled={keyPath === KeyPath.UNKNOWN}
-                  value={keyPath === KeyPath.UNKNOWN ? pubkeyDefaultValue : pubKeyInput}
-                  onChange={(value: string) => {
-                    setPubKeyInput(value);
-                  }}
-                />
-              </div>
-            </div>
-
-            <div className="compile-modal-item">
-              <div className="compile-modal-label">Tapleaf version:</div>
-              <RadioGroup
-                className="compile-modal-radio-group"
-                inline
-                value={tapleafVersion}
-                onChange={(value: TapleafVersion) => {
-                  setTapleafVersion(value);
-                }}
-              >
-                <Radio value={TapleafVersion.DEFAULT}>Default</Radio>
-                <Radio value={TapleafVersion.CUSTOM}>Custom</Radio>
-              </RadioGroup>
-
-              <Input
-                className="tapleaf-input"
-                disabled={tapleafVersion === TapleafVersion.DEFAULT}
-                value={tapleafVersion === TapleafVersion.DEFAULT ? tapleafDefaultValue : tapleafInput}
-                onChange={(value: string, event: React.SyntheticEvent<HTMLElement, Event>) => {
-                  setTapleafInput(value);
-                }}
-              />
-            </div>
-
-            <FormGroup>
-              <h6>Tweak Result</h6>
-              <div className="compile-modal-item">
-                <div className="compile-modal-label">Tweaked key:</div>
-                <InputGroup className="compile-modal-input-group">
-                  <Input value={tweakedResult} />
-                  <Whisper placement="top" trigger="click" speaker={<Tooltip>Text has been copied to clipboard!</Tooltip>}>
-                    <InputGroup.Button onClick={() => navigator.clipboard.writeText(tweakedResult)}>
-                      <Icon icon="copy" />
-                    </InputGroup.Button>
-                  </Whisper>
-                </InputGroup>
-              </div>
-              <div className="compile-modal-item">
-                <div className="compile-modal-label">ScriptPubkey:</div>
-                <InputGroup className="compile-modal-input-group">
-                  <Input value={tweakedResult} />
-                  <Whisper placement="top" trigger="click" speaker={<Tooltip>Text has been copied to clipboard!</Tooltip>}>
-                    <InputGroup.Button onClick={() => navigator.clipboard.writeText(tweakedResult)}>
-                      <Icon icon="copy" />
-                    </InputGroup.Button>
-                  </Whisper>
-                </InputGroup>
-              </div>
-              <div className="compile-modal-item">
-                <div className="compile-modal-label">Bech32 address:</div>
-                <InputGroup className="compile-modal-input-group">
-                  <Input value={tweakedResult} />
-                  <Whisper placement="top" trigger="click" speaker={<Tooltip>Text has been copied to clipboard!</Tooltip>}>
-                    <InputGroup.Button onClick={() => navigator.clipboard.writeText(tweakedResult)}>
-                      <Icon icon="copy" />
-                    </InputGroup.Button>
-                  </Whisper>
-                </InputGroup>
-              </div>
-            </FormGroup>
-          </Form>
-        </Modal.Body>
-        <Modal.Footer className="compile-modal-footer">
-          <Button onClick={() => setCompileModalData({ show: false })} appearance="primary">
-            Ok
-          </Button>
-        </Modal.Footer>
-      </Modal>
-
+      <CompileModal scriptWiz={scriptWiz} compileModalData={compileModalData} showCompileModal={(show) => setCompileModalData({ show })} />
       <ScriptEditorHeader compileButtonClick={compileScripts} />
       <div className="script-editor-main-div scroll">
         <div className="script-editor-container">
