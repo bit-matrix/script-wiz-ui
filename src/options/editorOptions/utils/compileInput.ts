4fbe04708bee9479b06b45c8d17e6e6ada602060
hüseyin sarıtaş <huseyinsaritas@test-MacBook-Pro.local>
2021-10-28 01:49:20 +0300

    compileInput func added

---
+++ src/options/editorOptions/utils/compileInput.ts
@@ -0,0 +1,21 @@
+import WizData from '@script-wiz/wiz-data';
+
+export const compileInput = (input: string): WizData => {
+  // HEX DATA INPUT
+  if (input.startsWith('0x')) {
+    const newInput = input.substr(2, input.length);
+    return WizData.fromHex(newInput);
+  }
+
+  if ((input.startsWith('"') && input.endsWith('"')) || (input.startsWith("'") && input.endsWith("'"))) {
+    const formattedInput = input.substr(1, input.length - 2);
+    return WizData.fromText(formattedInput);
+  }
+
+  // NUMBER INPUT
+  if (!isNaN(input as any)) {
+    return WizData.fromNumber(Number(input));
+  }
+  // eslint-disable-next-line no-throw-literal
+  throw 'Compile Final Input Error: it is not a valid final input string!';
+};
