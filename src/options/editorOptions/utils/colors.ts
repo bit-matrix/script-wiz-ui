2f09a7975113909337ccd1a05f5c59f556cf563f
hüseyin sarıtaş <huseyinsaritas@test-MacBook-Pro.local>
2021-10-27 15:21:31 +0300

    colors: lightSalmonPink added

--- src/options/editorOptions/utils/colors.ts
+++ src/options/editorOptions/utils/colors.ts
@@ -1,13 +1,14 @@
-const vibrantYellow = "ffd700";
-const subtleGray = "666677";
-const salmon = "d68d72";
-const lightBlue = "8addff";
-const blue = "3c9dda";
-const fuchsia = "d081c4";
-const oak = "d9daa2";
-const red = "ff0000";
-const mistBlue = "a8bcce";
-const lightOlive = "b5cea8";
-const darkOlive = "5bb498";
+const vibrantYellow = 'ffd700';
+const subtleGray = '666677';
+const salmon = 'd68d72';
+const lightBlue = '8addff';
+const blue = '3c9dda';
+const fuchsia = 'd081c4';
+const oak = 'd9daa2';
+const red = 'ff0000';
+const mistBlue = 'a8bcce';
+const lightOlive = 'b5cea8';
+const darkOlive = '5bb498';
+const lightSalmonPink = 'FF9AA2';
 
-export { vibrantYellow, subtleGray, salmon, lightBlue, blue, fuchsia, oak, red, mistBlue, lightOlive, darkOlive };
+export { vibrantYellow, subtleGray, salmon, lightBlue, blue, fuchsia, oak, red, mistBlue, lightOlive, darkOlive, lightSalmonPink };
