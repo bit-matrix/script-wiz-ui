16e87804fab771ed0a77da33fb6f2cb86d04479c
afarukcali <afarukcali@gmail.com>
2021-11-22 19:24:02 +0300

    editor options auto layout config.

--- src/options/editorOptions/editorOptions.ts
+++ src/options/editorOptions/editorOptions.ts
@@ -1,22 +1,23 @@
-import * as Monaco from "monaco-editor/esm/vs/editor/editor.api";
+import * as Monaco from 'monaco-editor/esm/vs/editor/editor.api';
 
 const editorOptions: Monaco.editor.IEditorConstructionOptions = {
-    cursorBlinking: "smooth",
-    dragAndDrop: true,
-    fontSize: 14,
-    lineHeight: 18,
-    fontFamily: "'Fira Mono', monospace",
-    scrollBeyondLastLine: false,
-    contextmenu: false,
-    folding: false,
-    wrappingIndent: "same",
-    minimap: { enabled: false },
-    scrollbar: {
-        verticalScrollbarSize: 6,
-        horizontalScrollbarSize: 6,
-        alwaysConsumeMouseWheel: false,
-        vertical: "hidden",
-    },
+  cursorBlinking: 'smooth',
+  dragAndDrop: true,
+  fontSize: 14,
+  lineHeight: 18,
+  fontFamily: "'Fira Mono', monospace",
+  scrollBeyondLastLine: false,
+  contextmenu: false,
+  folding: false,
+  wrappingIndent: 'same',
+  minimap: { enabled: false },
+  automaticLayout: true,
+  scrollbar: {
+    verticalScrollbarSize: 6,
+    horizontalScrollbarSize: 6,
+    alwaysConsumeMouseWheel: false,
+    vertical: 'hidden',
+  },
 };
 
 export default editorOptions;
