49716393acc7f0b42f0cd61d6477d5e84f018575
huseyinsaritas <hsynsrtss@gmail.com>
2021-11-24 17:32:36 +0300

    TransactionTemplateModal component

--- src/components/TransactionTemplateModal/TransactionTemplateModal.tsx
+++ src/components/TransactionTemplateModal/TransactionTemplateModal.tsx
@@ -1,19 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Button, Modal } from 'rsuite';
 import './TransactionTemplateModal.scss';
 
-type Props = {
-  modalData: { show: boolean; data?: string };
-  showModal: (show: boolean) => void;
+type TxInput = {
+  previousTxId: string;
+  vout: string;
+  sequence: string;
+  scriptPubKey: string;
+  amount: string;
+  assetId?: string;
 };
 
-const TransactionTemplateModal: React.FC<Props> = ({ modalData, showModal }) => {
+type TxOutput = {
+  scriptPubKey: string;
+  amount: string;
+  assetId?: string;
+};
+
+type Props = {
+  showModal: boolean;
+  showModalCallBack: (show: boolean) => void;
+};
+
+const TransactionTemplateModal: React.FC<Props> = ({ showModal, showModalCallBack }) => {
+  const [txInputs, setTxInputs] = useState<TxInput[]>([]);
+  const [txOutputs, setTxOutputs] = useState<TxOutput[]>([]);
   return (
-    <Modal size="lg" show={modalData.show} backdrop={false} onHide={() => showModal(false)}>
+    <Modal size="lg" show={showModal} backdrop={false} onHide={() => showModalCallBack(false)}>
       <Modal.Header />
-      <Modal.Body></Modal.Body>
+      <Modal.Body>
+        <div>
+          <div className="transaction-template-header">
+            <p>Inputs</p>
+            <p>Outputs</p>
+          </div>
+          <div className="transaction-template-main">
+            <div className="transaction-inputs">a</div>
+            <div className="vertical-line"></div>
+            <div className="transaction-outputs">b</div>
+          </div>
+        </div>
+      </Modal.Body>
       <Modal.Footer>
-        <Button onClick={() => showModal(false)} appearance="primary">
+        <Button onClick={() => showModalCallBack(false)} appearance="primary">
           Done
         </Button>
       </Modal.Footer>
