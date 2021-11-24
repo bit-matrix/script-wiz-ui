36b8e785da7dd9e0e0fb62d909977dedeca76ec8
huseyinsaritas <hsynsrtss@gmail.com>
2021-11-25 00:04:08 +0300

    TransactionOutput: index

--- src/components/TransactionTemplateModal/TransactionOutput/TransactionOutput.tsx
+++ src/components/TransactionTemplateModal/TransactionOutput/TransactionOutput.tsx
@@ -5,16 +5,17 @@ import './TransactionOutput.scss';
 const TransactionOutput = () => {
   return (
     <div className="tx-output-main">
+      <p className="tx-output-index">Index #0</p>
       <div className="tx-output-item">
-        <div className="compile-modal-label">scriptPubkey:</div>
+        <div className="tx-modal-label">scriptPubkey:</div>
         <Input onChange={(value: string) => {}} />
       </div>
       <div className="tx-output-item">
-        <div className="compile-modal-label">Amount:</div>
+        <div className="tx-modal-label">Amount:</div>
         <Input placeholder="8-bytes" onChange={(value: string) => {}} />
       </div>
       <div className="tx-output-item">
-        <div className="compile-modal-label">Asset ID:</div>
+        <div className="tx-modal-label">Asset ID:</div>
         <Input placeholder="32-bytes" onChange={(value: string) => {}} />
       </div>
     </div>
