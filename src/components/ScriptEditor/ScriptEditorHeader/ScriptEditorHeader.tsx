a202cd0a110d64fe4c7368d1558b731dc7e71275
huseyinsaritas <hsynsrtss@gmail.com>
2021-11-25 00:06:37 +0300

    ScriptEditorHeader: txTemplateClick props, tx template button style

--- src/components/ScriptEditor/ScriptEditorHeader/ScriptEditorHeader.tsx
+++ src/components/ScriptEditor/ScriptEditorHeader/ScriptEditorHeader.tsx
@@ -4,17 +4,17 @@ import './ScriptEditorHeader.scss';
 
 type Props = {
   compileButtonClick: () => void;
-  importTemplateClick: () => void;
+  txTemplateClick: () => void;
 };
 
-const ScriptEditorHeader: React.FC<Props> = ({ compileButtonClick, importTemplateClick }) => {
+const ScriptEditorHeader: React.FC<Props> = ({ compileButtonClick, txTemplateClick }) => {
   return (
     <div className="script-editor-headers-main">
       <div className="script-editor-header-sub-item editor">
         Editor
         <div className="compile-button">
           <Whisper placement="top" trigger="hover" speaker={<Tooltip>Import your transaction template</Tooltip>}>
-            <IconButton icon={<Icon icon="download" />} circle size="sm" onClick={importTemplateClick} />
+            <IconButton className="tx-template-icon" icon={<Icon icon="download" />} circle size="sm" onClick={txTemplateClick} />
           </Whisper>
           <Whisper placement="top" trigger="hover" speaker={<Tooltip>Compile your script</Tooltip>}>
             <IconButton icon={<Icon icon="terminal" />} circle size="sm" onClick={compileButtonClick} />
