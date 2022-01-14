31f98737559f34bb4495c0442e11726147e9a590
ayseren <ayse.eren@windowslive.com>
2022-01-14 18:51:50 +0300

    Helmet tag was added in App.js

---
+++ src/App.js
@@ -0,0 +1,26 @@
+/* eslint-disable no-template-curly-in-string */
+import React from 'react';
+import { AppRouter } from './router/AppRouter';
+import { Helmet } from 'react-helmet';
+import './App.scss';
+
+const App = () => {
+  return (
+    <div>
+      <Helmet>
+        <meta charset="utf-8" />
+        <link rel="icon" href="%PUBLIC_URL%/favicon.ico" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />           
+        <meta name="theme-color" content="#000000" />
+        <meta name="description" content="Script Wiz, Online Bitcoin Script Editor" />
+        <link rel="apple-touch-icon" href="%PUBLIC_URL%/logo192.png" />
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-XH7VQK0DKJ"></script>
+        <link rel="manifest" crossorigin="use-credentials" href="%PUBLIC_URL%/manifest.json" />
+        <title>Script Wiz</title>
+      </Helmet>
+      <AppRouter />
+    </div>
+  );
+};
+
+export default App;
