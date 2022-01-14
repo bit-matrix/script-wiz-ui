32bd9438a708d1f9c50e2c89ffb94867e3c3d5ba
ayseren <ayse.eren@windowslive.com>
2022-01-14 22:18:25 +0300

    some meta tags are added

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { Helmet } from 'react-helmet';
 import './App.scss';
 
 const App = () => {
+  const description = "Online Bitcoin Script Editor. Script Wizard makes it easy to design and compile custom Liquid scripts";
   return (
     <div>
       <Helmet>
@@ -12,7 +13,9 @@ const App = () => {
         <link rel="icon" href="%PUBLIC_URL%/favicon.ico" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />           
         <meta name="theme-color" content="#000000" />
-        <meta name="description" content="Script Wiz, Online Bitcoin Script Editor" />
+        <meta name="description" content={description} />
+        <meta name="author" content="https://www.linkedin.com/company/bitmatrix/" />
+        <meta name="robots" content="nofollow, noindex" />
         <link rel="apple-touch-icon" href="%PUBLIC_URL%/logo192.png" />
         <script async src="https://www.googletagmanager.com/gtag/js?id=G-XH7VQK0DKJ"></script>
         <link rel="manifest" crossorigin="use-credentials" href="%PUBLIC_URL%/manifest.json" />
