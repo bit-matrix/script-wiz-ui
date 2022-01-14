c5cb639d74539c59afece55f5cae8d3ace8f2dd9
ayseren <ayse.eren@windowslive.com>
2022-01-14 23:49:50 +0300

    rename App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+/* eslint-disable no-template-curly-in-string */
+import React from 'react';
+import { AppRouter } from './router/AppRouter';
+import './App.scss';
+
+const App = () => {
+  return <AppRouter />;
+};
+
+export default App;
