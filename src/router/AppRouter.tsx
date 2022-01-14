68a7678e05be326c11c0139fdd8a56ede760fa34
ayseren <ayse.eren@windowslive.com>
2022-01-14 21:41:39 +0300

    404 page creation

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -4,6 +4,7 @@ import { ROUTE_PATH } from './ROUTE_PATH';
 import { Helper } from '../pages/Helper/Helper';
 import { Home } from '../pages/Home/Home';
 import { SignatureTools } from '../pages/SignatureTools/SignatureTools';
+import FourZeroFour from '../components/FourZeroFour';
 
 export const AppRouter = (): JSX.Element => {
   return (
@@ -12,6 +13,7 @@ export const AppRouter = (): JSX.Element => {
         <Route exact path={ROUTE_PATH.HOME} component={Home} />
         <Route exact path={ROUTE_PATH.HELPER} component={Helper} />
         <Route exact path={ROUTE_PATH.SIGNATURE_TOOLS} component={SignatureTools} />
+        <Route exact path='*' component={FourZeroFour} />
       </Switch>
     </Router>
   );
