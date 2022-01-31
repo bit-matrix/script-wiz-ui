9260fc86b68107b09c7e8a6e0fc254630be45306
ayseren <ayse.eren@windowslive.com>
2022-01-31 18:51:54 +0300

    unused lines was removed

--- src/components/ScriptEditor/ScriptEditorInput/ScriptEditorInput.tsx
+++ src/components/ScriptEditor/ScriptEditorInput/ScriptEditorInput.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import React, { useEffect, useMemo, useState } from 'react';
+import './ScriptEditorInput.scss';
 
 import * as languageOptions from '../../../options/editorOptions/languageOptions';
 import * as Monaco from 'monaco-editor/esm/vs/editor/editor.api';
