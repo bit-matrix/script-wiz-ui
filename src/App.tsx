b830ce5d379673e972a670945ac939f7bd5c05d8
huseyinsaritas <hsynsrtss@gmail.com>
2022-01-31 17:40:30 +0300

    App: react mosaic styles changed.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable no-template-curly-in-string */
 import React from 'react';
 import { AppRouter } from './router/AppRouter';
+import 'react-mosaic-component/react-mosaic-component.css';
 import './App.scss';
 
 const App = (): JSX.Element => {
