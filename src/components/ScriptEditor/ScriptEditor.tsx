b3f9ba45769fd5ff546b6bbe32fbf5435f189b2b
afarukcali <afarukcali@gmail.com>
2022-02-14 16:22:42 +0300

    Script editor witness element set.

--- src/components/ScriptEditor/ScriptEditor.tsx
+++ src/components/ScriptEditor/ScriptEditor.tsx
@@ -190,21 +190,21 @@ const ScriptEditor: React.FC<Props> = ({ scriptWiz }) => {
         const inputTextValue = inputText.substring(1, inputText.length - 1);
 
         if (inputTextValue.startsWith('0x')) {
-          scriptWiz.parseHex(inputTextValue.substring(2), !isWitnessElement);
+          scriptWiz.parseHex(inputTextValue.substring(2), isWitnessElement);
         } else if (inputTextValue.startsWith('0b')) {
-          scriptWiz.parseBin(inputTextValue.substring(2), !isWitnessElement);
+          scriptWiz.parseBin(inputTextValue.substring(2), isWitnessElement);
         } else if (
           (inputTextValue.startsWith('"') && inputTextValue.endsWith('"')) ||
           (inputTextValue.startsWith("'") && inputTextValue.endsWith("'"))
         ) {
           const inputTextValueString = inputTextValue.substring(1, inputTextValue.length - 1);
-          scriptWiz.parseText(inputTextValueString, !isWitnessElement);
+          scriptWiz.parseText(inputTextValueString, isWitnessElement);
         } else if (!isNaN(Number(inputTextValue))) {
-          scriptWiz.parseNumber(Number(inputTextValue), !isWitnessElement);
+          scriptWiz.parseNumber(Number(inputTextValue), isWitnessElement);
         } else if (inputTextValue.startsWith('OP_')) {
-          if (isWitnessElement) {
+          if (!isWitnessElement) {
             const opwordToOphex = scriptWiz.opCodes.wordHex(inputTextValue);
-            scriptWiz.parseHex(opwordToOphex.substring(2), !isWitnessElement);
+            scriptWiz.parseHex(opwordToOphex.substring(2), isWitnessElement);
           } else {
             setErrorMessage('Unlocking bytecode may contain only push operations.');
           }
@@ -213,7 +213,7 @@ const ScriptEditor: React.FC<Props> = ({ scriptWiz }) => {
         }
       } else if (inputText.startsWith('OP_')) {
         if (isWitnessElement) {
-          scriptWiz.parseOpcode(inputText, !isWitnessElement);
+          scriptWiz.parseOpcode(inputText, isWitnessElement);
         } else {
           setErrorMessage('Unlocking bytecode may contain only push operations.');
         }
@@ -265,8 +265,10 @@ const ScriptEditor: React.FC<Props> = ({ scriptWiz }) => {
 
         if (line !== '') {
           if (i < firstEditorLineCount) {
+            console.log('1');
             parseInput(line, false);
           } else {
+            console.log('2');
             parseInput(line);
           }
 
