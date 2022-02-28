8dd174b903219f2723800554a24cc577e1097c3c
huseyinsaritas <hsynsrtss@gmail.com>
2022-02-28 23:11:38 +0300

    helper: getWhispers func

---
+++ src/helper/getWhispers.tsx
@@ -0,0 +1,11 @@
+import WizData from '@script-wiz/wiz-data';
+import CustomWhisper from '../components/ScriptEditor/CustomWhisper';
+
+export const getWhispers = (stackDataArray: WizData[], lineNumber?: number) =>
+  stackDataArray.map((stackData: WizData, index: number) => {
+    const key = `whisper-${lineNumber && lineNumber.toString()}-${index.toString()}-text`;
+    let displayValue = '0x' + stackData.hex;
+    if (stackData.number !== undefined) displayValue = stackData.number.toString();
+    else if (stackData.text !== undefined) displayValue = stackData.text;
+    return <CustomWhisper key={key} tooltip={stackData.hex} display={displayValue} label={stackData.label} />;
+  });
