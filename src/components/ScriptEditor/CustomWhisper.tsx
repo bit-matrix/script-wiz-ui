7238e99d112cefaabb444f81cc4dde357a1d2b46
huseyinsaritas <hsynsrtss@gmail.com>
2022-02-28 23:12:40 +0300

    CustomWhisper: label props

--- src/components/ScriptEditor/CustomWhisper.tsx
+++ src/components/ScriptEditor/CustomWhisper.tsx
@@ -4,12 +4,16 @@ import { getOutputValueType } from '../../helper';
 type Props = {
   tooltip: string;
   display: string;
+  label?: string;
 };
 
-const CustomWhisper: React.FC<Props> = ({ tooltip, display }) => {
+const CustomWhisper: React.FC<Props> = ({ tooltip, display, label }) => {
   return (
     <div className="tooltip">
-      <div className={`editor-output-text ${getOutputValueType(display)} `}>{display}</div>
+      <div className={`editor-output-text ${getOutputValueType(display)}`}>
+        {label ? <em className="editor-output-label">{label} </em> : ''}
+        {display}
+      </div>
       <span className="tooltiptext">{'0x' + tooltip}</span>
     </div>
   );
