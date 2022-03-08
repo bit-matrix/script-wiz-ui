31a56e724f7c4136fcaab1ad21d7a0e7f3ac0855
Ayse Gul Eren <ayse.eren@windowslive.com>
2022-03-08 18:37:50 +0300

    Tooltip css

--- src/helper/getWhispers.tsx
+++ src/helper/getWhispers.tsx
@@ -1,10 +1,11 @@
 import WizData from '@script-wiz/wiz-data';
-import CustomWhisper from '../components/ScriptEditor/CustomWhisper';
+import CustomWhisper from '../components/CustomWhisper/CustomWhisper';
 
 export const getWhispers = (stackDataArray: WizData[], lineNumber?: number) =>
   stackDataArray.map((stackData: WizData, index: number) => {
     const key = `whisper-${lineNumber && lineNumber.toString()}-${index.toString()}-text`;
     let displayValue = '0x' + stackData.hex;
+
     if (stackData.number !== undefined) displayValue = stackData.number.toString();
     else if (stackData.text !== undefined) displayValue = stackData.text;
 
