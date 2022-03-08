ef74f4c4eeb47e99436d37fb160e6fbbbd174b66
Ayse Gul Eren <ayse.eren@windowslive.com>
2022-03-08 17:14:30 +0300

    CustomWhisper: label parameter was added

--- src/components/ScriptEditor/CustomWhisper.tsx
+++ src/components/ScriptEditor/CustomWhisper.tsx
@@ -1,15 +1,21 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { getOutputValueType } from '../../helper';
+import WizData from '@script-wiz/wiz-data';
 
 type Props = {
+  key: string;
   tooltip: string;
   display: string;
+  label?: string;
 };
 
-const CustomWhisper: React.FC<Props> = ({ tooltip, display }) => {
+const CustomWhisper: React.FC<Props> = ({ key, tooltip, display, label }) => {
   return (
-    <div className="tooltip">
-      <div className={`editor-output-text ${getOutputValueType(display)} `}>{display}</div>
+    <div className="tooltip" key={key}>
+      <div className={`editor-output-text ${getOutputValueType(display)} `}>
+        {label ? <em className="editor-output-label">{label} </em> : ''}
+        {display}
+      </div>
       <span className="tooltiptext">{'0x' + tooltip}</span>
     </div>
   );
