c5d4d50675688b5a210b303a4695c4a07fc0c379
afarukcali <afarukcali@gmail.com>
2022-04-12 01:14:48 +0300

    Mast tool show control block

--- src/pages/MastTool/MastTool.tsx
+++ src/pages/MastTool/MastTool.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { taproot, TAPROOT_VERSION } from '@script-wiz/lib-core';
-import { Button, Form, Input, InputGroup, RadioGroup, Tooltip, Whisper } from 'rsuite';
+import { Button, Col, Form, Grid, Input, InputGroup, RadioGroup, Row, Tooltip, Whisper, Modal } from 'rsuite';
 import Radio, { ValueType } from 'rsuite/esm/Radio/Radio';
 import WizData from '@script-wiz/wiz-data';
 import { validHex } from '../../utils/helper';
@@ -13,6 +13,7 @@ export const MastTool: React.FC = () => {
   const [innerKey, setInnerkey] = useState<string>('');
   const [scripts, setScripts] = useState<string[]>(['']);
   const [taprootResult, setTaprootResult] = useState<Taproot>();
+  const [controlBlockModal, setControlBlockModal] = useState<{ show: boolean; data: string }>({ show: false, data: '' });
 
   const calculateTaprootResult = (versionInput?: TAPROOT_VERSION) => {
     const innerKeyWiz = WizData.fromHex(innerKey);
@@ -20,6 +21,19 @@ export const MastTool: React.FC = () => {
     const result = taproot.tapRoot(innerKeyWiz, scriptsWiz, versionInput || version);
 
     setTaprootResult(result);
+
+    return result;
+  };
+
+  const showControlBlock = (index: number) => {
+    const scriptsWiz = scripts.map((sc) => WizData.fromHex(sc));
+    let vs = 'c0';
+
+    if (version === TAPROOT_VERSION.LIQUID) vs = 'c4';
+
+    const result = taproot.controlBlockCalculation(scriptsWiz, vs, innerKey, index);
+
+    setControlBlockModal({ show: true, data: result });
   };
 
   return (
@@ -52,19 +66,38 @@ export const MastTool: React.FC = () => {
       </div>
       {scripts.map((sc, index) => {
         return (
-          <div key={index} className="signature-tools-result-item">
+          <div key={index} className="mast-tools-result-item">
             <h6 className="signature-tools-tab-header">Leaf {index + 1} (Hex)</h6>
-            <Input
-              className="signature-tools-main-input"
-              type="text"
-              value={sc}
-              placeholder="0x"
-              onChange={(value: string) => {
-                const clonedScripts = [...scripts];
-                clonedScripts[index] = value.replace(/\s/g, '');
-                setScripts(clonedScripts);
-              }}
-            />
+            <Grid fluid>
+              <Row className="show-grid">
+                <Col xs={20}>
+                  <Input
+                    className="signature-tools-main-input"
+                    type="text"
+                    value={sc}
+                    placeholder="0x"
+                    onChange={(value: string) => {
+                      const clonedScripts = [...scripts];
+                      clonedScripts[index] = value.replace(/\s/g, '');
+                      setScripts(clonedScripts);
+                    }}
+                  />
+                </Col>
+                <Col xs={4}>
+                  <Button
+                    className="mast-tools-button"
+                    appearance="primary"
+                    size="md"
+                    onClick={() => {
+                      showControlBlock(index);
+                    }}
+                    disabled={!validHex(innerKey)}
+                  >
+                    Show Control Block
+                  </Button>
+                </Col>
+              </Row>
+            </Grid>
           </div>
         );
       })}
@@ -149,6 +182,39 @@ export const MastTool: React.FC = () => {
           </Form.Group>
         </Form>
       )}
+
+      <Modal
+        size="sm"
+        open={controlBlockModal.show}
+        backdrop={false}
+        onClose={() => {
+          setControlBlockModal({ ...controlBlockModal, show: false });
+        }}
+      >
+        <Modal.Body>
+          <div className="compile-modal-item">
+            <div className="compile-modal-label">Control Block :</div>
+            <InputGroup className="compile-modal-input-group">
+              <Input value={controlBlockModal.data} />
+              <Whisper placement="top" trigger="click" speaker={<Tooltip>Text has been copied to clipboard!</Tooltip>}>
+                <InputGroup.Button onClick={() => navigator.clipboard.writeText(controlBlockModal.data)}>
+                  <CopyIcon width="1rem" height="1rem" />
+                </InputGroup.Button>
+              </Whisper>
+            </InputGroup>
+          </div>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button
+            onClick={() => {
+              setControlBlockModal({ ...controlBlockModal, show: false });
+            }}
+            appearance="primary"
+          >
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </div>
   );
 };
