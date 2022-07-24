1d67bc08cba3cb38d4e18f8565870eb956074620
Ayse Gul Eren <ayse.eren@windowslive.com>
2022-07-24 16:08:03 +0300

    TransactionInputsContainer  draft creation

---
+++ src/components/TransactionTemplateModalV2/TransactionInputsContainer/TransactionInputsContainer.tsx
@@ -0,0 +1,27 @@
+import { FC } from 'react';
+import { Button } from 'rsuite';
+import TransactionInput from '../TransactionInput/TransactionInput';
+import './TransactionInputsContainer.scss';
+
+type Props = {
+  lastBlock: any;
+  version: string;
+};
+
+const TransactionInputsContainer: FC<Props> = ({ lastBlock, version }) => {
+  return (
+    <div className="tx-inputs-container">
+      <div className="tx-inputs-container-header">
+        <p>Inputs</p>
+      </div>
+
+      <TransactionInput lastBlock={lastBlock} version={version} />
+
+      <Button className="tx-inputs-container-button" onClick={() => {}}>
+        + Add New Input
+      </Button>
+    </div>
+  );
+};
+
+export default TransactionInputsContainer;
