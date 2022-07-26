112a649e33d58a718f62020972d9f9ea193da131
Ayse Gul Eren <ayse.eren@windowslive.com>
2022-07-26 19:37:29 +0300

    TransactionOutputsContainer: prop type was changed

--- src/components/TransactionTemplateModalV2/TransactionOutputsContainer/TransactionOutputsContainer.tsx
+++ src/components/TransactionTemplateModalV2/TransactionOutputsContainer/TransactionOutputsContainer.tsx
@@ -4,14 +4,18 @@ import { Button } from 'rsuite';
 import TransactionOutput from '../TransactionOutput/TransactionOutput';
 import './TransactionOutputsContainer.scss';
 
-const TransactionOutputsContainer: FC = () => {
+type Props = {
+  txOutputOnChange: (value: TxOutput) => void;
+};
+
+const TransactionOutputsContainer: FC<Props> = ({ txOutputOnChange }) => {
   return (
     <div className="tx-outputs-container">
       <div className="tx-outputs-container-header">
         <p>Outputs</p>
       </div>
 
-      <TransactionOutput txOutputOnChange={(value: TxOutput) => console.log(value)} />
+      <TransactionOutput txOutputOnChange={(value: TxOutput) => txOutputOnChange(value)} />
 
       <Button className="tx-outputs-container-button" onClick={() => {}}>
         + Add New Output
