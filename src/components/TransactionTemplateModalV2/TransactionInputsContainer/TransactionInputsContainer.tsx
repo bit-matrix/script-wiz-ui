7ab64b7e891935bfec1179f9e0085e14f60acca0
Ayse Gul Eren <ayse.eren@windowslive.com>
2022-07-26 20:28:51 +0300

    TransactionInputsContainer: addition multiple tx inputs, index, currentInputIndex fix

--- src/components/TransactionTemplateModalV2/TransactionInputsContainer/TransactionInputsContainer.tsx
+++ src/components/TransactionTemplateModalV2/TransactionInputsContainer/TransactionInputsContainer.tsx
@@ -1,24 +1,108 @@
-import { FC } from 'react';
-import { TxInput } from '@script-wiz/lib-core';
-import { Button } from 'rsuite';
+import { FC, useState } from 'react';
+import { TxInputLiquid } from '@script-wiz/lib-core';
+import { Button, Radio } from 'rsuite';
 import TransactionInput from '../TransactionInput/TransactionInput';
+import { ScriptWiz } from '@script-wiz/lib';
 import './TransactionInputsContainer.scss';
+import CloseIcon from '../../Svg/Icons/Close';
 
 type Props = {
   lastBlock: any;
   version: string;
+  blockHeight: string;
+  blockTimestamp: string;
+  txInputOnChange: (value: any) => void;
 };
 
-const TransactionInputsContainer: FC<Props> = ({ lastBlock, version }) => {
+const txInputInitial = {
+  previousTxId: '',
+  vout: '',
+  sequence: '',
+  scriptPubKey: '',
+  amount: '',
+  assetId: '',
+  blockHeight: '',
+  blockTimestamp: '',
+  confidental: false,
+};
+
+const TransactionInputsContainer: FC<Props> = ({ lastBlock, version, blockHeight, blockTimestamp, txInputOnChange }) => {
+  const [txInputs, setTxInputs] = useState<TxInputLiquid[]>([txInputInitial]);
+  const [currentInputIndex, setCurrentInputIndex] = useState<number>(0);
+
+  const txInputsOnChange = (input: TxInputLiquid, index: number, isCurrentInputIndex: boolean) => {
+    const newTxInputs = [...txInputs];
+    const relatedInputIndex = txInputs.findIndex((input, i) => i === index);
+
+    const newInput = {
+      previousTxId: input.previousTxId,
+      vout: input.vout,
+      sequence: input.sequence,
+      scriptPubKey: input.scriptPubKey,
+      amount: input.amount,
+      assetId: input.assetId,
+      confidental: input.confidental,
+    };
+
+    newTxInputs[relatedInputIndex] = newInput;
+    setTxInputs(newTxInputs);
+
+    if (isCurrentInputIndex) setCurrentInputIndex(index);
+  };
+
   return (
     <div className="tx-inputs-container">
       <div className="tx-inputs-container-header">
         <p>Inputs</p>
       </div>
+      {txInputs.map((input: TxInputLiquid, index: number) => {
+        const txInput = { input, index, checked: currentInputIndex === index };
+        return (
+          <div className="tx-inputs-container-box">
+            <div className="tx-inputs-container-index">Index #{txInput.index}</div>
 
-      <TransactionInput lastBlock={lastBlock} version={version} txInputOnChange={(value: TxInput) => console.log(value)} />
+            <Radio
+              onChange={(value: any, checked: boolean) => {
+                txInputsOnChange(txInput.input, txInput.index, checked);
+              }}
+              value={txInput.index}
+              checked={txInput.checked}
+            >
+              Current Input Index
+            </Radio>
+            <div
+              className="tx-inputs-container-close-icon"
+              onClick={() => {
+                const newTxInputs = [...txInputs];
+                if (txInputs.length > 1) {
+                  newTxInputs.splice(txInput.index, 1);
+                  setTxInputs(newTxInputs);
+                }
+              }}
+            >
+              <CloseIcon width="1rem" height="1rem" />
+            </div>
 
-      <Button className="tx-inputs-container-button" onClick={() => {}}>
+            <TransactionInput
+              lastBlock={lastBlock}
+              version={version}
+              blockHeight={blockHeight}
+              blockTimestamp={blockTimestamp}
+              txInputOnChange={txInputsOnChange}
+              txInput={{ input: txInput.input, index: txInput.index, isCurrentInputIndex: txInput.checked }}
+            />
+          </div>
+        );
+      })}
+      <Button
+        className="tx-inputs-container-button"
+        onClick={() => {
+          const newTxInput = txInputInitial;
+          const newTxInputs = [...txInputs];
+          newTxInputs.push(newTxInput);
+          setTxInputs(newTxInputs);
+        }}
+      >
         + Add New Input
       </Button>
     </div>
