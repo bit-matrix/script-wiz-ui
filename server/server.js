ff824430ed7a8d14986249ccf657cf091160d574
afarukcali <afarukcali@gmail.com>
2022-08-22 15:09:10 +0300

    server review.

--- server/server.js
+++ server/server.js
@@ -1,18 +1,16 @@
-import express from 'express';
-import fs from 'fs';
-import path from 'path';
-
-import React from 'react';
-import ReactDOMServer from 'react-dom/server';
-
 import App from '../src/App.tsx';
 
-import sha256Streaming from '@bitmatrix/sha256streaming';
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const React = require('react');
+const ReactDOMServer = require('react-dom/server');
+const sha256Streaming = require('@bitmatrix/sha256streaming');
 
-import sslRedirect from 'heroku-ssl-redirect';
+// import sslRedirect from 'heroku-ssl-redirect';
 
 const app = express();
-app.use(sslRedirect());
+// app.use(sslRedirect());
 
 app.use('^/$', (req, res, next) => {
   fs.readFile(path.resolve('./build/index.html'), 'utf-8', (err, data) => {
