0eeb3604af7d35b9531741cd62b4632d48af5807
afarukcali <afarukcali@gmail.com>
2022-10-07 02:59:33 +0300

    ssr review.

--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { CustomProvider } from 'rsuite';
+import { ScriptWiz, VM } from '@script-wiz/lib';
+import App from './App';
 
 declare global {
   interface Window {
-    extension: any;
+    vm: VM;
+    scriptWiz?: ScriptWiz;
+    setVm: (vm: VM) => void;
   }
 }
 
-const props = window.extension;
+const vm = window.vm;
+const scriptWiz = JSON.parse(window.scriptWiz) as unknown as ScriptWiz;
+const setVm = window.setVm;
+
+console.log('scriptwiz', scriptWiz);
 
 ReactDOM.hydrate(
   <React.StrictMode>
     <CustomProvider theme="dark">
-      <App extension={props} />
+      <App setVm={setVm} vm={vm} scriptWiz={scriptWiz} />
     </CustomProvider>
   </React.StrictMode>,
   document.getElementById('root'),
