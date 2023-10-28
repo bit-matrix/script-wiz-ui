692b52fa4191302ed4f769874090587ebe4c9880
katsu <0x0ff@onsats.org>
2023-10-28 21:15:28 +0200

    bump versions + change router after version bump

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { ROUTE_PATH } from './ROUTE_PATH';
 import { Helper } from '../pages/Helper/Helper';
 import { Home } from '../pages/Home/Home';
@@ -13,16 +13,16 @@ import { BitCalculator } from '../pages/256BitCalculator/256BitCalculator';
 export const AppRouter = (): JSX.Element => {
   return (
     <Router>
-      <Switch>
-        <Route exact path={ROUTE_PATH.HOME} component={Home} />
-        <Route exact path={ROUTE_PATH.HELPER} component={Helper} />
-        <Route exact path={ROUTE_PATH.SIGNATURE_TOOLS} component={SignatureTools} />
-        <Route exact path={ROUTE_PATH.TAPLEAF_CALCULATOR} component={TapLeafCalculator} />
-        <Route exact path={ROUTE_PATH.MAST_TOOL} component={MastTool} />
-        <Route exact path={ROUTE_PATH.SHA256D} component={Sha256Midstate} />
-        <Route exact path={ROUTE_PATH.EC_CALCULATOR} component={EcCalculator} />
-        <Route exact path={ROUTE_PATH.CALCULATOR} component={BitCalculator} />
-      </Switch>
+      <Routes>
+        <Route path={ROUTE_PATH.HOME} element={<Home />} />
+        <Route path={ROUTE_PATH.HELPER} element={<Helper />} />
+        <Route path={ROUTE_PATH.SIGNATURE_TOOLS} element={<SignatureTools />} />
+        <Route path={ROUTE_PATH.TAPLEAF_CALCULATOR} element={<TapLeafCalculator />} />
+        <Route path={ROUTE_PATH.MAST_TOOL} element={<MastTool />} />
+        <Route path={ROUTE_PATH.SHA256D} element={<Sha256Midstate />} />
+        <Route path={ROUTE_PATH.EC_CALCULATOR} element={<EcCalculator />} />
+        <Route path={ROUTE_PATH.CALCULATOR} element={<BitCalculator />} />
+      </Routes>
     </Router>
   );
 };
